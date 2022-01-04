c430bf9690a6029dc624759d39febe2e1a7cf59a
ooooo5h <ooooo5h91@gmail.com>
2022-01-04 12:18:16 +0900

    클래스의 메쏘드 추가해서 나이계산기능을 받아서 활용

--- app/src/main/java/com/neppplus/recyclerview_20220104/adapters/StudentAdapter.kt
+++ app/src/main/java/com/neppplus/recyclerview_20220104/adapters/StudentAdapter.kt
@@ -33,7 +33,7 @@ class StudentAdapter(
 //            학생 데이터를 하나 받으면, 텍스트뷰에 연결해주는 함수
             txtName.text = data.name
             txtAddress.text = data.address
-            txtAge.text = "(${data.birthYear}세)"
+            txtAge.text = "(${data.getAge()}세)"
         }
     }
 
--- app/src/main/java/com/neppplus/recyclerview_20220104/models/Student.kt
+++ app/src/main/java/com/neppplus/recyclerview_20220104/models/Student.kt
@@ -5,4 +5,12 @@ class Student(
     val birthYear: Int,
     val address: String
 ) {
+
+
+//    2022년 기준 나이를 리턴해주는 함수
+
+    fun getAge() : Int {
+
+        return 2022 - this.birthYear + 1
+    }
 }
