03648b0ba370ab1e87a11505e4aa8f70c90e70c4
ooooo5h <ooooo5h91@gmail.com>
2022-01-04 11:12:43 +0900

    리싸이클러뷰의 어댑터를 상속받는 클래스를 추가   - 단순 상속만 명시, 그 외 작업 X

---
+++ app/src/main/java/com/neppplus/recyclerview_20220104/adapters/StudentAdapter.kt
@@ -0,0 +1,9 @@
+package com.neppplus.recyclerview_20220104.adapters
+
+import androidx.recyclerview.widget.RecyclerView
+
+// 학생들 한 명 데이터 + 학생 한 명의 xml을 조합해서 => 위치에 맞게 보내주는 역할
+// 안드로이드에서 제공하는 기초 작업을 상속받아서 일부만 수정하자
+
+class StudentAdapter : RecyclerView.Adapter<>() {
+}
