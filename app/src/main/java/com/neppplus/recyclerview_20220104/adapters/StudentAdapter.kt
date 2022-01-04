2d29784f2bb6c837f6bb033085ab8a3e96d581d0
ooooo5h <ooooo5h91@gmail.com>
2022-01-04 11:47:59 +0900

    목록의 갯수와 어떤 xml을 표시할건지 설정 마무리   - 리싸이클러뷰 어댑터의 기초 작업 마무리

--- app/src/main/java/com/neppplus/recyclerview_20220104/adapters/StudentAdapter.kt
+++ app/src/main/java/com/neppplus/recyclerview_20220104/adapters/StudentAdapter.kt
@@ -1,9 +1,11 @@
 package com.neppplus.recyclerview_20220104.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.neppplus.recyclerview_20220104.R
 import com.neppplus.recyclerview_20220104.models.Student
 
 // 학생들 한 명 데이터 + 학생 한 명의 xml을 조합해서 => 위치에 맞게 보내주는 역할
@@ -21,6 +23,11 @@ class StudentAdapter(
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+//        1. 한 줄을 표현하는 xml을 가지고, 코틀린의 변수에 담아두자(inflate)
+        val row = LayoutInflater.from(mContext).inflate(R.layout.student_list_item, parent, false)
+
+//        2. row를 MyViewHolder로 감싸서 리턴
+        return MyViewHolder(row)
 
     }
 
